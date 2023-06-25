e649a3a6971bf395928e7388d36559804cc0dfd9
LinFeng <1183671661@qq.com>
2023-06-25 13:17:46 +0800

    题库

---
+++ js/public.js
@@ -0,0 +1,5 @@
+const JSONList = [
+    { id: "2019", name: '数据库原理与应用_单选题', describe: '数据库单选题', file: "数据库_单选题.json" },
+    { id: "2020", name: '数据库原理与应用_判断题', describe: '数据库判断题', file: "数据库_判断题.json" },
+    { id: "2021", name: '数据库原理与应用_填空题', describe: '数据库填空题', file: "数据库_填空题.json" }
+]
